241c616946e43cb1ef7560fee730c603a45b94eb
Alberto Ponces <ponces26@gmail.com>
2023-05-16 14:36:21 +0100

    fix: Improve HTTP response handling

--- app/src/main/java/me/phh/treble/app/UpdaterActivity.kt
+++ app/src/main/java/me/phh/treble/app/UpdaterActivity.kt
@@ -125,11 +125,20 @@ class UpdaterActivity : PreferenceActivity() {
                 }
                 override fun onResponse(call: Call, response: Response) {
                     Log.e("PHH", "Got response")
-                    otaJson = JSONTokener(response.body?.string()).nextValue() as JSONObject
-                    runOnUiThread(Runnable {
-                        hasUpdate = existsUpdate()
-                        updateUiElements(false)
-                    })
+                    if ((response.code == 200 || response.code == 304) && response.body != null) {
+                        Log.e("PHH", "Response body: " + response.body.string())
+                        otaJson = JSONTokener(response.body?.string()).nextValue() as JSONObject
+                        runOnUiThread(Runnable {
+                            hasUpdate = existsUpdate()
+                            updateUiElements(false)
+                        })
+                    } else {
+                        Log.e("PHH", "Invalid HTTP response or body. Code: " + response.code)
+                        runOnUiThread(Runnable {
+                            hasUpdate = false
+                            updateUiElements(false)
+                        })
+                    }
                 }
             })
         } else {
