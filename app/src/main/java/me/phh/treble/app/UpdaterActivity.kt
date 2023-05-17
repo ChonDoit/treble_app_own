968684e1159c1cfb5e25672ffc1b36fbeb835864
Pierre-Hugues Husson <phh@phh.me>
2023-05-17 20:28:14 +0200

    Fix build -- was somehow broken by 241c61

--- app/src/main/java/me/phh/treble/app/UpdaterActivity.kt
+++ app/src/main/java/me/phh/treble/app/UpdaterActivity.kt
@@ -126,7 +126,7 @@ class UpdaterActivity : PreferenceActivity() {
                 override fun onResponse(call: Call, response: Response) {
                     Log.e("PHH", "Got response")
                     if ((response.code == 200 || response.code == 304) && response.body != null) {
-                        Log.e("PHH", "Response body: " + response.body.string())
+                        Log.e("PHH", "Response body: " + response.body?.string())
                         otaJson = JSONTokener(response.body?.string()).nextValue() as JSONObject
                         runOnUiThread(Runnable {
                             hasUpdate = existsUpdate()
