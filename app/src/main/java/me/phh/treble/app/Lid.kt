ca266fdde999b5164786ffc3f27576280f74cfec
Pierre-Hugues Husson <phh@phh.me>
2024-05-31 11:55:45 +0200

    Add magnetic back cover for Legion Y700 -- Manual merge of PR #32

--- app/src/main/java/me/phh/treble/app/Lid.kt
+++ app/src/main/java/me/phh/treble/app/Lid.kt
@@ -28,7 +28,7 @@ object Lid: EntryStartup {
 
     fun lenovo(ctxt: Context) {
         val sensorManager = ctxt.getSystemService(SensorManager::class.java)
-        val sensors = listOf("ah1902 Hall Effect Sensor Wakeup", "bu52053nvx Hall Effect Sensor Wakeup")
+        val sensors = listOf("ah1902 Hall Effect Sensor Wakeup", "bu52053nvx Hall Effect Sensor Wakeup", "mmc56x3x Hall Effect Sensor Wakeup")
         val lidSensor = sensorManager.getSensorList(Sensor.TYPE_ALL).firstOrNull() { sensor -> sensors.any { name -> sensor.name.contains(name)}}
         if(lidSensor == null) {
             Log.d("PHH", "Failed finding sensor for lid wakeup")
@@ -85,7 +85,10 @@ object Lid: EntryStartup {
     }
 
     override fun startup(ctxt: Context) {
-        if(Tools.vendorFpLow.startsWith("Lenovo/TB-9707F_PRC/TB-9707F".lowercase()) || Tools.vendorFpLow.startsWith("Lenovo/LenovoTB-J716F_PRC/J716F".lowercase())) {
+        if(Tools.vendorFpLow.startsWith("Lenovo/TB-9707F_PRC/TB-9707F".lowercase()) ||
+                Tools.vendorFpLow.startsWith("Lenovo/LenovoTB-J716F_PRC/J716F".lowercase()) ||
+                Tools.vendorFpLow.startsWith("Lenovo/TB320FC".lowercase())
+                ) {
             lenovo(ctxt)
         }
         if(Tools.vendorFpLow.startsWith("Cat/S22FLIP/S22FLIP".lowercase())) {
